2bc6ca2e6e87c396539345731d6ea38145289b4b
yarotoskin-crypto <yaro.toskin@yandex.ru>
2026-04-26 15:15:33 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/test1/MainActivity.kt
@@ -0,0 +1,7 @@
+// android/app/src/main/kotlin/com/example/test1/MainActivity.kt
+package com.example.test1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
